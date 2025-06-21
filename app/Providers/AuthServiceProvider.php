b1755e81fe61032677b35c9839939688f5c3652e
Chandran Sajeewan <sajeewan01@gmail.com>
2025-06-21 22:50:28 +0530

    Set Up Policy for Authorization

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Task;
+use App\Policies\TaskPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+
+class AuthServiceProvider extends ServiceProvider
+{
+
+    protected $policies = [
+        Task::class => TaskPolicy::class,
+    ];
+    
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+       $this->registerPolicies();
+      
+    }
+}
